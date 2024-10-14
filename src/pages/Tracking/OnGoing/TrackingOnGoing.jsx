8793a215e3d551a359ea609e8409e3a44598f726
Kevinnn1701 <kevinwijaya1701@gmail.com>
2024-10-15 00:27:37 +0700

    tracking

--- src/pages/Tracking/OnGoing/TrackingOnGoing.jsx
+++ src/pages/Tracking/OnGoing/TrackingOnGoing.jsx
@@ -1,188 +1,260 @@
-import { Col, Form, Input, Modal, Row, Select, Table } from 'antd'
-import React, { useState } from 'react'
-import { CustomPagination } from '../../../components/ui/Table/CustomPagination'
+import { Col, Form, Input, Modal, Row, Select, Table, message } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { CustomPagination } from '../../../components/ui/Table/CustomPagination';
 import { trackingColumns } from '../../../components/ui/Table/columns/tracking';
 import { PlusOutlined } from '@ant-design/icons';
 import Button from '../../../components/ui/Button/Button';
 import { CiEdit, CiTrash } from 'react-icons/ci';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../../components/ui/Loading/Loading';
+import { apiRequest } from '../../../utils/api';
 
 const TrackingOnGoing = () => {
-    const { Option } = Select;
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const [form] = Form.useForm()
+  const { Option } = Select;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+  const [data, setData] = useState([]);
+  const [pagination, setPagination] = useState({ page: 1, perPage: 10, totalData: 1 });
+  const [search, setSearch] = useState('');
+  const [order, setOrder] = useState({
+    key: 'id',
+    order: 'desc',
+  });
 
-    const dataSource = [
-        {
-            key: '1',
-            number: '1',
-            nama: 'Tes',
-            serialCode: 'Tes',
-            badanUsaha: 'Lorem',
-            jenisLegalitas: 'Lorem',
-        },
-        {
-            key: '2',
-            number: '2',
-            nama: 'Tes',
-            serialCode: 'Tes',
-            badanUsaha: 'Lorem',
-            jenisLegalitas: 'Lorem',
-        },
-        {
-            key: '3',
-            number: '3',
-            nama: 'Tes',
-            serialCode: 'Tes',
-            badanUsaha: 'Lorem',
-            jenisLegalitas: 'Lorem',
-        },
-        {
-            key: '4',
-            number: '4',
-            nama: 'Tes',
-            serialCode: 'Tes',
-            badanUsaha: 'Lorem',
-            jenisLegalitas: 'Lorem',
-        },
-    ];
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await apiRequest('get', '/order', {}, {
+        page: pagination.page,
+        perPage: pagination.perPage,
+        where: search,
+        orderBy: `${order.key}:${order.order}`,
+        status: 'PROCESS'
+      });
 
-    const columnsWithActions = [
-        ...trackingColumns,
-        {
-            title: 'Action',
-            key: 'action',
-            width: 100,
-            align: 'center',
-            render: (text, record) => (
-                <div className="flex justify-center items-center gap-2">
-                    <CiEdit className="text-2xl text-center text-second cursor-pointer hover:text-main"
+      setData(
+        response.data.data.map((item) => ({
+          ...item,
+          key: item.id,
+        }))
+      );
+
+      setPagination({
+        page: response.data.meta.currentPage,
+        perPage: response.data.meta.perPage,
+        totalData: response.data.meta.total,
+      });
+    } catch (error) {
+      message.error(error.response?.data?.message || 'Error fetching data');
+    }
+    setLoading(false);
+  };
+
+  const handleEdit = async (id, values) => {
+    setLoading(true);
+    try {
+      const response = await apiRequest('patch', `/order/${id}`, {
+        status: values.status,
+      });
+      if (response.status === 200) {
+        message.success('Data Updated');
+        fetchData();
+        Modal.destroyAll()
+      }
+    } catch (error) {
+      message.error(error.response?.data?.message || 'Error updating data');
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [pagination.page, pagination.perPage, search]);
+
+  const columnsWithActions = [
+    ...trackingColumns,
+    {
+      title: 'Action',
+      key: 'action',
+      width: 100,
+      align: 'center',
+      render: (text, record) => (
+        <div className="flex justify-center items-center gap-2">
+          <CiEdit
+            className="text-2xl text-center text-second cursor-pointer hover:text-main"
+            onClick={() => {
+              Modal.info({
+                title: 'Edit Data',
+                centered: true,
+                content: (
+                  <Form
+                    form={form}
+                    initialValues={{ status: record.status }}
+                    onFinish={(values) => handleEdit(record.id, values)}
+                  >
+                    <Row>
+                      <Col span={24}>
+                        <Form.Item name="status" label="Status">
+                          <Select value={"PROCESS"}>
+                            <Option value="PENDING">PENDING</Option>
+                            <Option value="PROCESS">ON GOING</Option>
+                            <Option value="FINISH">FINISH</Option>
+                            <Option value="CANCEL">FAILED</Option>
+                          </Select>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+
+                    <div className="mt-5 flex justify-end">
+                      <Button
+                        type="default"
+                        className="mr-2"
                         onClick={() => {
-                            Modal.info({
-                                title: 'Edit Data',
-                                centered: true,
-                                content: (
-                                    <Form form={form}>
-                                        <Row>
-                                            <Col span={24}>
-                                                <Form.Item
-                                                    name="status"
-                                                    label="Status"
-                                                >
-                                                    <Select defaultValue="PROCESS">
-                                                        <Select.Option value="PENDING">PENDING</Select.Option>
-                                                        <Select.Option value="PROCESS">ON GOING</Select.Option>
-                                                        <Select.Option value="FINISH">FINISH</Select.Option>
-                                                        <Select.Option value="CANCEL">FAILED</Select.Option>
-                                                    </Select>
-                                                </Form.Item>
-                                            </Col>
-                                        </Row>
-
-                                        <div className="mt-5 flex justify-end">
-                                            <Button type="default" className="mr-2" onClick={() => {
-                                                form.resetFields()
-                                                Modal.destroyAll()
-                                                }}
-                                            >Cancel</Button>
-                                            <Button type="primary" className='bg-main'>Save</Button>
-                                        </div>
-                                    </Form>
-                                ),
-                                footer: null
-                            });
+                          form.resetFields();
+                          Modal.destroyAll();
                         }}
-                    />
-                    <CiTrash className="text-2xl text-center text-second cursor-pointer hover:text-main" 
-                        onClick={() => {
-                            Modal.info({
-                                title: 'Delete Data',
-                                centered: true,
-                                content: (
-                                    <React.Fragment>
-                                        <div>Are you sure want to delete this data?</div>
-                                        <div className="mt-5 flex justify-end">
-                                            <Button type="default" className="mr-2" onClick={() => {
-                                                Modal.destroyAll()
-                                            }}>Cancel</Button>
-                                            <Button type="primary" className='bg-main'>Delete</Button>
-                                        </div>
-                                    </React.Fragment>
-                                ),
-                                footer: null
-                            });
-                        }}
-                    />
-                </div>
-            ),
-        },
-    ];
-    return (
-        <React.Fragment>
-            <Row className="w-full">
-                <Col span={24}>
-                    <div className="rounded-lg">
-                        <Row>
-                            <Col span={24}>
-                                <div className="flex flex-col">
-                                    <div className="bg-white p-5 rounded-lg">
-                                        <Row>
-                                            <Col span={24}>
-                                                <div className="text-[24px] text-main inline-block">Tracking On Going</div>
-                                                <br />
-                                                <span className="text-[15px] inline-block mt-5">Tracking on going data</span>
-                                            </Col>
-                                        </Row>
-                                    </div>
-
-                                    <div className="mt-5 p-5 bg-white border rounded-lg">
-                                        <Row>
-                                            <Col span={24}>
-                                                <div className="flex flex-col">
-                                                    <Row justify="space-between" align="middle" className="mb-4">
-                                                        <Col>
-                                                            <span className="text-[24px] inline-block">On Going</span>
-                                                        </Col>
-                                                        <Col className="flex gap-2">
-                                                            <Input.Search placeholder="Search..." />
-                                                            <Button type="primary" onClick={() => {
-                                                                window.location.href = 'form-order'
-                                                            }}>
-                                                                Add New
-                                                                <PlusOutlined />
-                                                            </Button>
-                                                        </Col>
-                                                    </Row>
-
-                                                    <Table
-                                                        dataSource={dataSource}
-                                                        columns={columnsWithActions}
-                                                        pagination={{ pageSize: 10, position: ['bottomCenter'], showSizeChanger: true, style: { display: "none" } }}
-                                                        bordered
-                                                        scroll={{ x: 768 }}
-                                                    />
-                                                    <CustomPagination
-                                                        data={dataSource}
-                                                        pagination={{ page: 1, perPage: 10, totalData: 1 }}
-                                                        changeLimit={() => { }}
-                                                        changePage={() => { }}
-                                                    />
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </div>
-
-                                </div>
-                            </Col>
-                        </Row>
+                      >
+                        Cancel
+                      </Button>
+                      <Button type="primary" htmlType="submit" className="bg-main">
+                        Save
+                      </Button>
                     </div>
-                </Col>
-            </Row>
-            <Loading loading={loading} />
-        </React.Fragment>
-    )
-}
+                  </Form>
+                ),
+                footer: null,
+              });
+            }}
+          />
+          {/* <CiTrash
+            className="text-2xl text-center text-second cursor-pointer hover:text-main"
+            onClick={() => {
+              Modal.info({
+                title: 'Delete Data',
+                centered: true,
+                content: (
+                  <React.Fragment>
+                    <div>Are you sure you want to delete this data?</div>
+                    <div className="mt-5 flex justify-end">
+                      <Button
+                        type="default"
+                        className="mr-2"
+                        onClick={() => {
+                          Modal.destroyAll();
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        type="primary"
+                        className="bg-main"
+                        onClick={() => handleDelete(record.id)}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </React.Fragment>
+                ),
+                footer: null,
+              });
+            }}
+          /> */}
+        </div>
+      ),
+    },
+  ];
 
-export default TrackingOnGoing
+  return (
+    <React.Fragment>
+      <Row className="w-full">
+        <Col span={24}>
+          <div className="rounded-lg">
+            <Row>
+              <Col span={24}>
+                <div className="flex flex-col">
+                  <div className="bg-white p-5 rounded-lg">
+                    <Row>
+                      <Col span={24}>
+                        <div className="text-[24px] text-main inline-block">Tracking On Going</div>
+                        <br />
+                        <span className="text-[15px] inline-block mt-5">Tracking On Going data</span>
+                      </Col>
+                    </Row>
+                  </div>
+
+                  <div className="mt-5 p-5 bg-white border rounded-lg">
+                    <Row>
+                      <Col span={24}>
+                        <div className="flex flex-col">
+                          <Row justify="space-between" align="middle" className="mb-4">
+                            <Col>
+                              <span className="text-[24px] inline-block">On Going</span>
+                            </Col>
+                            <Col className="flex gap-2">
+                              <Input.Search
+                                placeholder="Search..."
+                                onSearch={(value) => setSearch(value)}
+                              />
+                              <Button
+                                type="primary"
+                                onClick={() => {
+                                  navigate('form-order');
+                                }}
+                              >
+                                Add New
+                                <PlusOutlined />
+                              </Button>
+                            </Col>
+                          </Row>
+
+                          <Table
+                            dataSource={data}
+                            columns={columnsWithActions}
+                            pagination={{
+                              current: pagination.page,
+                              pageSize: pagination.perPage,
+                              total: pagination.totalData,
+                              onChange: (page, pageSize) => {
+                                setPagination({
+                                  ...pagination,
+                                  page,
+                                  perPage: pageSize,
+                                });
+                              },
+                            }}
+                            bordered
+                            scroll={{ x: 768 }}
+                          />
+                          <CustomPagination
+                            data={data}
+                            pagination={pagination}
+                            changeLimit={(perPage) => {
+                              setPagination({
+                                ...pagination,
+                                perPage,
+                              });
+                            }}
+                            changePage={(page) => {
+                              setPagination({
+                                ...pagination,
+                                page,
+                              });
+                            }}
+                          />
+                        </div>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+      <Loading loading={loading} />
+    </React.Fragment>
+  );
+};
+
+export default TrackingOnGoing;
